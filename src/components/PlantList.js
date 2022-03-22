7bb342ed53094823104da7c6342aa6d86995d011
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-03-22 16:33:59 -0600

    commit tree f005d86ff1ea710b16b4527ccb0e508ff1e56866

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,13 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({displayPlants}) {
+  const showPlants = displayPlants.map(item => {
+    return <PlantCard key={item.id} name={item.name} price={item.price} image={item.image}/>
+  })
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{showPlants}</ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [isPlant, setIsPlant] = useState([])
+  const [isSearch, setIsSearch] = useState('')
+
+  useEffect(() =>{
+    fetch("http://localhost:6001/plants")
+    .then(resp => resp.json())
+    .then(item => setIsPlant(item))
+  },[])
+
+  function handleAddPlant(newPlant){
+    setIsPlant([...isPlant, newPlant])
+  }
+
+  function handleSearch(item){
+    setIsSearch(item.toUpperCase())
+  }
+
+  const itemsToDisplay = isPlant.filter(item =>{
+
+     return item.name.toUpperCase().startsWith(isSearch)
+  })
+  console.log(isSearch)
+  console.log(isPlant)
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant}/>
+      <Search handleSearch={handleSearch}/>
+      <PlantList displayPlants={itemsToDisplay}/>
     </main>
   );
 }
